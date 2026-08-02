cedc5e417ac6d97043c72dd237123ff086240f8b
rfroiman <rfroiman@hotmail.com>
2026-08-02 13:42:45 +0300

    Day 1 - Python Data Types

---
+++ Week1/Day1/Samples.py
@@ -0,0 +1,38 @@
+print(2+5)
+
+print(10%3)
+
+print(int(10/3))
+
+print(10*3)
+
+print(10**3)
+
+print(abs(-5))
+
+print(abs(5))
+
+print(float(5))
+
+#num=int(input("Enter a number: "))
+#list=[1,2,3,4,5]
+#print (num==5)
+#if num==5:
+#    print("num is equal to 5")
+#else:
+#    print("num is not equal to 5")
+
+#print(num in list)
+
+string="Count how many t have in this string"
+count=0 
+for letter in string:
+    if letter=="t":
+        count+=1
+print("The letter 't' appears", count, "times in the string.")  
+
+
+age=input("Enter your age: ")
+print(f"You have more {100 - int(age)} years to reach 100 years old.")
+        
+        
