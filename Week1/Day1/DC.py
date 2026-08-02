f867e3fdf5ae87a15413b6cb94a13f7648c1d12a
rfroiman <rfroiman@hotmail.com>
2026-08-02 15:44:39 +0300

    Daily Challenge 1

--- Week1/Day1/DC.py
+++ Week1/Day1/DC.py
@@ -2,15 +2,17 @@
 
 import random
 
+while True:
+    
+ string=input("Enter a string at least 10 characters long: ")
 
-string=input("Enter a string at least 10 characters long: ")
-
-if len(string) < 10:
-    print("String not long enough.")        
-elif len(string) > 10:
+ if len(string) < 10:
+    print("String not long enough.") 
+ elif len(string) > 10:
     print("String too long.")
-else:
+ else:
     print("Perfect string")
+    break
 
 print("The first character of the string is:", string[0])
 print("The last character of the string is:", string[-1])   
